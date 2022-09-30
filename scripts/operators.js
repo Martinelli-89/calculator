3d000aa2e9dd2bb9c0f701ad03d73cca516ed820
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-09-30 12:54:19 +0100

    added second display for holding temp numbers

---
+++ scripts/operators.js
@@ -0,0 +1,12 @@
+//Getting values from both displays
+const tempDisplay = document.querySelector(".tempDisplay");
+const display = document.querySelector(".result");
+
+const handleCalcolation = (event) => {
+
+    //Nothing happens if not values have been entered 
+    if(tempDisplay.innerText =="" && display.innerText=="") {
+        return;
+    }
+
+}
